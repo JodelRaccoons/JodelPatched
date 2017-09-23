e71c4e0780315fa7ff4496871f3b1cf6283a0258
krokofant <krokofant@users.noreply.github.com>
2017-09-23 14:00:12 +0200

    Use different syntax

--- patched/src/main/java/com/jodelapp/jodelandroidv3/AppModule.java
+++ patched/src/main/java/com/jodelapp/jodelandroidv3/AppModule.java
@@ -2,24 +2,16 @@ package com.jodelapp.jodelandroidv3;
 
 import com.squareup.otto.Bus;
 
-import lanchon.dexpatcher.annotation.DexAction;
 import lanchon.dexpatcher.annotation.DexAdd;
 import lanchon.dexpatcher.annotation.DexEdit;
-import lanchon.dexpatcher.annotation.DexIgnore;
 import lanchon.dexpatcher.annotation.DexWrap;
 
-@DexEdit(staticConstructorAction = DexAction.ADD)
+@DexEdit
 public class AppModule {
-
-    @DexIgnore
-    public AppModule() {
-    }
 
     @DexAdd
     public static Bus staticBus;
 
-    static {
-    }
 
     @DexWrap
     private Bus createBus() {
--- patched/src/main/java/com/jodelapp/jodelandroidv3/JodelApp.java
+++ patched/src/main/java/com/jodelapp/jodelandroidv3/JodelApp.java
@@ -4,19 +4,15 @@ import android.annotation.SuppressLint;
 import android.app.Application;
 import android.content.Context;
 
-import lanchon.dexpatcher.annotation.DexAction;
 import lanchon.dexpatcher.annotation.DexAdd;
 import lanchon.dexpatcher.annotation.DexEdit;
 import lanchon.dexpatcher.annotation.DexWrap;
 
-@DexEdit(staticConstructorAction = DexAction.ADD)
+@DexEdit
 public class JodelApp extends Application {
 
     @DexAdd
     public static Context staticContext;
-
-    static {
-    }
 
     @SuppressLint("MissingSuperCall")
     @DexWrap
